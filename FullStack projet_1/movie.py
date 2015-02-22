0c95325a9be283a3f072fc37700a5658acfd07c5
mcisko <mcisko@yahoo.fr>
2015-02-22 13:07:26 +0300

    Update
    Add commnents

--- FullStack projet_1/movie.py
+++ FullStack projet_1/movie.py
@@ -1,12 +1,21 @@
 
 
 class Movie():
+    """ The purpose of this code is to create a Movie Class. The class has four fields
+    title, movie_trailer, movie_storyline and movie_image.
+    Title is to store the title of the movie, movie_trailer is the
+    youtube link of the movie's trailer, story_line will hold a brief description of the movie
+    and finally movie_image will hold the poster of the movie
+"""
 
+    # creation of the constructor
     def __init__(self,title,movie_trailer,movie_storyline, movie_image):
         self.title = title
         self.movie_trailer = movie_trailer
         self.movie_image = movie_image
         self.movie_storyline = movie_storyline
+
+
 
 
 
